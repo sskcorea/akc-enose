74a60b019b4649db2016d0af48f86f3a42a8c95b
Slki Shin <slki.shin@samsung.com>
2018-07-06 08:24:13 +0000

    add azure connection
    Signed-off-by: Slki Shin <slki.shin@samsung.com>

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
+'use strict';
+
 var Enose = require('./enose.js').Enose;
 var enose = new Enose(1, 0x10);
 
 var Gpio = require('onoff').Gpio;
 var gpio = new Gpio(128, 'in', 'rising');
 
+// azure cloud
+var connectionString = 'HostName=artik-test.azure-devices.net;DeviceId=a530s;SharedAccessKey=sNp80pyTv+4La2nyedfff/9LN6CEwWnCiWRUB6mgdws=';
+var Mqtt = require('azure-iot-device-mqtt').Mqtt;
+var DeviceClient = require('azure-iot-device').Client
+var Message = require('azure-iot-device').Message;
+var client = DeviceClient.fromConnectionString(connectionString, Mqtt);
+
+// artik cloud
 var Config = require('./config.json');
 var ArtikCloud = require('artikcloud-js');
-var client = ArtikCloud.ApiClient.instance;
-var artikcloud_oauth = client.authentications['artikcloud_oauth'];
+var akc_client = ArtikCloud.ApiClient.instance;
+var artikcloud_oauth = akc_client.authentications['artikcloud_oauth'];
     artikcloud_oauth.accessToken = Config['device_token'];
 var api = new ArtikCloud.MessagesApi();
-var message = new ArtikCloud.Message();
+var akc_message = new ArtikCloud.Message();
 
 var data = {
     "temperature": 0,
@@ -21,34 +31,49 @@ var data = {
     "toluene2": 0,
 };
 
+function printResultFor(op) {
+    return function printResult(err, res) {
+        if (err) console.log('<azure ' + op + ' error: ' + err.toString());
+        if (res) console.log('<azure ' + op + ' status: ' + res.constructor.name);
+    };
+}
+
 var callback = function(err, data, resp) {
     if (err)
-        console.error('< ' + err);
+        console.error('<akc ' + err);
 }
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function senddata(enose) {
+function senddata2akc(enose) {
     data.temperature = enose.temperature;
     data.humidity = enose.humidity;
     data.acetone = enose.acetone;
     data.toluene1 = enose.toluene1;
     data.toluene2 = enose.toluene2;
 
-    message['data'] = data;
-    message['sdid'] = Config['device_id'];
+    akc_message['data'] = data;
+    akc_message['sdid'] = Config['device_id'];
 
-    console.log("> send message: " + JSON.stringify(message));
-    api.sendMessage(message, callback);
+    console.log('akc> send message: ' + JSON.stringify(akc_message));
+    api.sendMessage(akc_message, callback);
+}
+
+function senddata2azure(enose) {
+    var data = JSON.stringify({ temperature: enose.temperature, humidity: enose.humidity });
+    var azure_message = new Message(data);
+    console.log('azure> send message: ' + azure_message.getData());
+    client.sendEvent(azure_message, printResultFor('send'));
 }
 
 async function demo() {
-    await sleep(5000);
+    await sleep(2000);
     enose.init();
     enose.read();
-    senddata(enose);
+    senddata2akc(enose);
+    senddata2azure(enose);
     enose.clear();
 }
 
